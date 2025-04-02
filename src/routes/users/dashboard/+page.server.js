bc919ba507e4b6caae7e76be83df9a97a33e4eec
khannedy <echo.khannedy@gmail.com>
2025-04-03 00:05:42 +0700

    Add user authentication flow with login and dashboard routes
    Introduced a login page to capture user input and a dashboard page to display the logged-in user's information. Implemented cookie-based session management with support for login, logout, and access redirection.

---
+++ src/routes/users/dashboard/+page.server.js
@@ -0,0 +1,19 @@
+import {redirect} from "@sveltejs/kit";
+
+export async function load({cookies, url}) {
+
+  // logout
+  if (url.searchParams.get("logout")) {
+    cookies.delete("user", {path: "/"})
+    redirect(303, "/users/login")
+  }
+
+  // cookies not exists
+  if (!cookies.get("user")) {
+    redirect(303, "/users/login")
+  }
+
+  return {
+    user: cookies.get("user")
+  }
+}
