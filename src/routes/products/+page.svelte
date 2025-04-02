c2b76e281335018c496aab97a94a7fede4f22955
khannedy <echo.khannedy@gmail.com>
2025-04-02 23:29:05 +0700

    Add products route with layout and navigation updates
    Introduced a new `/products` route with pages to list products and display details for individual products. Updated navigation to include the "Products" link and added layout scripts to handle data fetching and rendering.

---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import {onMount} from "svelte";
+  import {invalidateAll} from "$app/navigation";
+
+  const {data} = $props();
+
+  onMount(() => {
+    invalidateAll()
+  })
+</script>
+
+<h1>Products</h1>
+
+<ul>
+    {#each data.products as product}
+        <li>
+            <a href="/products/{product}">{product}</a>
+        </li>
+    {/each}
+</ul>
