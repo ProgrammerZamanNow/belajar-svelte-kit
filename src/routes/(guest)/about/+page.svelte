8b707dec963092f615a2e0cd35147aafbfbc9220
khannedy <echo.khannedy@gmail.com>
2025-04-03 16:30:06 +0700

    Refactor route structure to use grouped directory conventions
    Moved existing routes into `(default)` and added new `(guest)` routes to enhance organization and clarity. This includes updates to layouts and pages for both groups for better management of route contexts.

---
+++ src/routes/(guest)/about/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome</h1>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis eaque, itaque iusto laborum natus odio odit, perferendis porro quae quo reprehenderit repudiandae sunt suscipit, veniam voluptate. Facilis numquam, voluptatibus! Officiis.</p>
