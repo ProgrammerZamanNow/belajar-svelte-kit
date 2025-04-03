72ef416fff69a6c299cc4072b9cf4332ae6afa8f
khannedy <echo.khannedy@gmail.com>
2025-04-03 17:03:17 +0700

    Add routing hooks and initialization functions
    Introduce `hooks.server.js` and `hooks.client.js` to handle server-side and client-side initialization and error management. Update URL rerouting logic and refine language parameter handling in routes for improved functionality.

---
+++ src/hooks.server.js
@@ -0,0 +1,10 @@
+export async function handle({event, resolve}) {
+
+  console.log(`receive request : ${event.url.href}`);
+
+  return await resolve(event);
+}
+
+export function init(){
+  console.log("init application");
+}
