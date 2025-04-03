be12e4a0d7b58901c0b0fd8773a09f0566281d73
khannedy <echo.khannedy@gmail.com>
2025-04-03 14:13:50 +0700

    Switch to server-side layout for profile with auth check
    The layout file was updated to run on the server side and include a user authentication check using cookies. Users without a valid cookie are now redirected to the login page. This enhances security by verifying user login status before loading the profile page.

---
+++ src/routes/profile/+layout.server.js
@@ -0,0 +1,15 @@
+import {redirect} from "@sveltejs/kit";
+
+export function load({cookies}) {
+  const user = cookies.get("user")
+  if (!user) {
+    redirect(
+      303,
+      "/users/login"
+    )
+  }
+
+  return {
+    user: user
+  }
+}
