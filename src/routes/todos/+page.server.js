a98fa41d87a545231a9786076cc7d952230ac04d
khannedy <echo.khannedy@gmail.com>
2025-04-03 15:29:03 +0700

    Add todo management with server-side actions
    Implemented server-side actions to add and delete todos dynamically. Integrated form handling with validation for adding todos and updated the frontend to reflect these changes. Commented out unused client-side loading logic to align with the new server-driven approach.

--- src/routes/todos/+page.server.js
+++ src/routes/todos/+page.server.js
@@ -1,7 +1,35 @@
-export async function load(){
+import {fail} from "@sveltejs/kit";
+
+let lastId = 0;
+let todos = [];
+
+export async function load() {
   return {
-    todos: [
-      "From Server"
-    ]
+    todos: todos
+  }
+}
+
+export const actions = {
+  add: async ({request}) => {
+    const data = await request.formData();
+    const todo = data.get("todo");
+
+    if (todo.trim() === "") {
+      return fail(400, {
+        error: true,
+        message: "Todo can not be empty"
+      })
+    }
+
+    todos.push({
+      id: lastId++,
+      name: todo
+    })
+  },
+
+  delete: async ({request}) => {
+    const data = await request.formData();
+    const id = data.get("id");
+    todos = todos.filter(item => item.id !== Number(id))
   }
 }
