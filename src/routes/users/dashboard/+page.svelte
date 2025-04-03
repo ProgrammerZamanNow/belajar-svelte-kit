13f9c2db306820622a28871d254bc44ddcc43a9a
khannedy <echo.khannedy@gmail.com>
2025-04-03 13:58:11 +0700

    Refactor user authentication and session management.
    Rewrote login and dashboard flows to use API endpoints for user session handling instead of relying on query parameters and server redirects. Introduced `/api/users/login` and `/api/users/current` endpoints to manage user authentication and session data more cleanly and asynchronously. Updated the frontend to align with the new API-driven approach.

--- src/routes/users/dashboard/+page.svelte
+++ src/routes/users/dashboard/+page.svelte
@@ -1,7 +1,29 @@
 <script>
-    const {data} = $props();
+  import {goto} from "$app/navigation";
+
+  const {data} = $props();
+
+  async function current() {
+    const response = await fetch("/api/users/current");
+    if (response.ok) {
+      return await response.json();
+    } else {
+      await goto("/users/login")
+    }
+  }
+
+  async function logout() {
+    await fetch("/api/users/current", {
+      method: "DELETE"
+    });
+
+    await goto("/users/login");
+  }
 </script>
 
-<h1>Hello {data.user}</h1>
-
-<a href="/users/dashboard?logout=true">Logout</a>
+{#await current()}
+    Loading user
+{:then data}
+    <h1>Hello {data.user}</h1>
+    <a href="#" onclick={logout}>Logout</a>
+{/await}
